0e1a48688941644db11441040036a8e694627273
Anwar Faizal <pejalhebat@gmail.com>
2023-06-14 09:14:16 +0800

    Init commit of distance-calculator

---
+++ src/main/java/com/pejal/distance/distancecalculator/DistanceService.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pejal.distance.distancecalculator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author pejalhebat
+ */
+
+@Slf4j
+@RestController
+@RequestMapping("/distance")
+public class DistanceService {
+    
+}
